c9bf94289a5da86012652becd4efa1d23fce9290
m4tcha <dionmanohms@hotmail.com>
2026-04-06 22:13:29 +0700

    remove unnecessary function and class

--- src/server/auth/password.ts
+++ src/server/auth/password.ts
@@ -1,21 +1,5 @@
 import argon2 from 'argon2'
 
-export class PasswordPolicyError extends Error {
-  constructor(message: string) {
-    super(message)
-    this.name = 'PasswordPolicyError'
-  }
-}
-
-export function validatePasswordPolicy(password: string): void {
-  if (password.length < 15) {
-    throw new PasswordPolicyError('Password must be at least 15 characters')
-  }
-  if (password.length > 128) {
-    throw new PasswordPolicyError('Password must be at most 128 characters')
-  }
-}
-
 export async function hashPassword(plain: string): Promise<string> {
   return argon2.hash(plain, {
     type: argon2.argon2id,
